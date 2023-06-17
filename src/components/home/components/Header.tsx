185a3fbf16d56314de43d9b7539fc1dbd5b747e7
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-17 22:43:19 +0530

    Add language select menu.

--- src/components/home/components/Header.tsx
+++ src/components/home/components/Header.tsx
@@ -1,6 +1,14 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Language } from "@mui/icons-material";
+import { AppBar, MenuItem, Select, Toolbar, SelectChangeEvent, InputAdornment } from "@mui/material";
+import { useState } from "react";
 
 function Header(): JSX.Element {
+    const [language, setLanguage] = useState<string>('English');
+
+    function changeLanguage(event: SelectChangeEvent<string>) {
+        setLanguage(event.target.value);
+    }
+
     return (
         <AppBar sx={{ backgroundColor: 'black' }}>
             <Toolbar>
@@ -9,6 +17,16 @@ function Header(): JSX.Element {
                     alt="Netflix logo."
                     style={{ width: 200, height: 80, objectFit: 'cover', marginLeft: '120px' }}
                 />
+                <Select
+                    startAdornment={<InputAdornment position='start'><Language /></InputAdornment>}
+                    id='language'
+                    value={language}
+                    onChange={changeLanguage}
+                    sx={{ height: 40, marginLeft: 80 }}
+                >
+                    <MenuItem value='English'>English</MenuItem>
+                    <MenuItem value='Hindi'>Hindi</MenuItem>
+                </Select>
             </Toolbar>
         </AppBar>
     );
