1e5770cb03cec1aa27f97a73640e4d0896fe3d95
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-17 22:16:53 +0530

    Add Netflix logo.

--- src/components/home/components/Header.tsx
+++ src/components/home/components/Header.tsx
@@ -1,8 +1,16 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
 function Header(): JSX.Element {
     return (
-        <>
-            
-        </>
+        <AppBar sx={{ backgroundColor: 'black' }}>
+            <Toolbar>
+                <img 
+                    src="src/assets/netflix logo.png" 
+                    alt="Netflix logo."
+                    style={{ width: 200, height: 80, objectFit: 'cover', marginLeft: '120px' }}
+                />
+            </Toolbar>
+        </AppBar>
     );
 }
 
