0412716437249cfafd5754d7dbc0136d5eeb081b
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-18 20:47:37 +0530

    Add flexbox properties.

--- src/components/home/components/EnterEmail.tsx
+++ src/components/home/components/EnterEmail.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardHeader } from "@mui/material";
 
 function EnterEmail(): JSX.Element {
     return (
-        <Card sx={{marginTop: 12}}>
+        <Card sx={{height: '100vh', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
             <CardHeader title='Unlimited movies, TV shows and more' subheader='Watch anywhere. Cancel anytime.'/>
             <CardContent>Ready to watch? Enter your email to create or restart your membership.</CardContent>
         </Card>
