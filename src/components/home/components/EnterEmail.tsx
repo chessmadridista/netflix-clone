fd858a57659985e040df5eedb26173f403e639a7
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-18 21:14:11 +0530

    Modify subheader color.

--- src/components/home/components/EnterEmail.tsx
+++ src/components/home/components/EnterEmail.tsx
@@ -1,11 +1,15 @@
-import { Card, CardContent, CardHeader } from "@mui/material";
+import { Card, CardContent, CardHeader, Typography } from "@mui/material";
 
 function EnterEmail(): JSX.Element {
     return (
         <>
-            <img src="src/assets/EnterEmail.jpg" alt="Enter email background image." style={{ marginLeft: 0, position: 'absolute', width: '100%'}} />
+            <img src="src/assets/EnterEmail.jpg" alt="Enter email background image." style={{ marginLeft: 0, position: 'absolute', height: '100%', width: '100%', opacity: '0.6', filter: 'blur(1px)'}} />
             <Card sx={{position: 'relative', height: '100vh', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: 'transparent', color: '#fff'}}>
-                <CardHeader title='Unlimited movies, TV shows and more' subheader='Watch anywhere. Cancel anytime.' />
+                <CardHeader title='Unlimited movies, TV shows and more' subheader={
+                <Typography variant="subtitle1" color="#fff">
+                    Watch anywhere. Cancel anytime.
+                </Typography>
+            } />
                 <CardContent>Ready to watch? Enter your email to create or restart your membership.</CardContent>
             </Card>
         </>
