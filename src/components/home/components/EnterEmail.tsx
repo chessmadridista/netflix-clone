6395ce192359cb5a60fa4950dd0c85add23a83eb
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-18 21:09:55 +0530

    Modify body default margin.

--- src/components/home/components/EnterEmail.tsx
+++ src/components/home/components/EnterEmail.tsx
@@ -2,10 +2,13 @@ import { Card, CardContent, CardHeader } from "@mui/material";
 
 function EnterEmail(): JSX.Element {
     return (
-        <Card sx={{height: '100vh', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-            <CardHeader title='Unlimited movies, TV shows and more' subheader='Watch anywhere. Cancel anytime.'/>
-            <CardContent>Ready to watch? Enter your email to create or restart your membership.</CardContent>
-        </Card>
+        <>
+            <img src="src/assets/EnterEmail.jpg" alt="Enter email background image." style={{ marginLeft: 0, position: 'absolute', width: '100%'}} />
+            <Card sx={{position: 'relative', height: '100vh', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: 'transparent', color: '#fff'}}>
+                <CardHeader title='Unlimited movies, TV shows and more' subheader='Watch anywhere. Cancel anytime.' />
+                <CardContent>Ready to watch? Enter your email to create or restart your membership.</CardContent>
+            </Card>
+        </>
     );
 }
 
