0ded2550c7eda015cd9a426c9aaf4cd7a4820c10
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-21 17:18:49 +0530

    Change font size.

--- src/components/home/components/EnterEmail.tsx
+++ src/components/home/components/EnterEmail.tsx
@@ -5,12 +5,23 @@ function EnterEmail(): JSX.Element {
         <>
             <img src="src/assets/EnterEmail.jpg" alt="Enter email background image." style={{ position: 'absolute', height: '100%', width: '100%', opacity: '0.6', filter: 'blur(1px)'}} />
             <Card sx={{position: 'relative', height: '100vh', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: 'transparent', color: '#fff'}}>
-                <CardHeader title='Unlimited movies, TV shows and more' subheader={
-                <Typography variant="subtitle1" color="#fff">
-                    Watch anywhere. Cancel anytime.
-                </Typography>
-            } />
-                <CardContent>Ready to watch? Enter your email to create or restart your membership.</CardContent>
+                <CardHeader 
+                    // title='Unlimited movies, TV shows and more' 
+                    title={
+                        <Typography variant="h3" sx={{fontWeight: 'bold'}}>
+                            Unlimited movies, TV shows and more
+                        </Typography>
+                    }
+                    subheader={
+                    <Typography variant="h5" color="#fff">
+                        Watch anywhere. Cancel anytime.
+                    </Typography>
+                } />
+                <CardContent>
+                    <Typography variant="h6">
+                        Ready to watch? Enter your email to create or restart your membership.
+                    </Typography>
+                </CardContent>
             </Card>
         </>
     );
