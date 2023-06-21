7b19c5af9976e19d7b09bdb0d5983a13d083233d
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-22 01:08:18 +0530

    Add card details.

--- src/components/home/components/EnjoyOnYourTv.tsx
+++ src/components/home/components/EnjoyOnYourTv.tsx
@@ -1,8 +1,22 @@
+import { Card, CardHeader, Typography } from "@mui/material";
+
 function EnjoyOnYourTv(): JSX.Element {
     return (
-        <>
-        
-        </>
+        <Card sx={{marginTop: 1, backgroundColor: '#000', color: '#fff', height: '100vh', display: 'flex', }}>
+            <CardHeader 
+                title={
+                    <Typography variant="h3">
+                        Enjoy on your TV
+                    </Typography>
+                }
+                subheader={
+                    <Typography variant="h6">
+                        Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.
+                    </Typography>
+                }
+            
+            />
+        </Card>
     );
 }
 
