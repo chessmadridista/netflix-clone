9c2015438f4fa2ec606229e933c733abbf02bb68
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-22 01:11:49 +0530

    Modify padding and margin.

--- src/components/home/components/EnjoyOnYourTv.tsx
+++ src/components/home/components/EnjoyOnYourTv.tsx
@@ -2,15 +2,15 @@ import { Card, CardHeader, Typography } from "@mui/material";
 
 function EnjoyOnYourTv(): JSX.Element {
     return (
-        <Card sx={{marginTop: 1, backgroundColor: '#000', color: '#fff', height: '100vh', display: 'flex', }}>
+        <Card sx={{marginTop: 1, paddingLeft: 5, backgroundColor: '#000', color: '#fff', height: '100vh', display: 'flex', }}>
             <CardHeader 
                 title={
-                    <Typography variant="h3">
+                    <Typography variant="h3" sx={{fontWeight: 'bolder'}}>
                         Enjoy on your TV
                     </Typography>
                 }
                 subheader={
-                    <Typography variant="h6">
+                    <Typography variant="h5" sx={{width: 500, marginTop: 2}}>
                         Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.
                     </Typography>
                 }
