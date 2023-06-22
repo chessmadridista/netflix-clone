eb1689caf362ddd03567b24abd2fd5c4d717c9a8
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-22 18:09:43 +0530

    Add content for kids.

--- src/components/home/components/KidProfiles.tsx
+++ src/components/home/components/KidProfiles.tsx
@@ -1,8 +1,34 @@
+import { Grid, Card, CardHeader, CardMedia, Typography } from "@mui/material";
+
 function KidProfiles(): JSX.Element {
     return (
-        <>
-        
-        </>
+        <Grid container sx={{marginTop: 1, backgroundColor: '#000', height: '100vh'}}>
+            <Grid item xs={6} sx={{display: 'flex', alignItems: 'center'}}>
+                <Card sx={{backgroundColor: '#000',}}>
+                    <CardMedia 
+                    component='img'
+                    src='src/assets/kid profiles.png'
+                    />
+
+                </Card>
+            </Grid>
+            <Grid item xs={6} sx={{display: 'flex', alignItems: 'center'}}>
+                <Card sx={{color: '#fff', backgroundColor: '#000'}}>
+                    <CardHeader
+                        title={
+                            <Typography variant="h3" sx={{fontWeight: 'bolder'}}>
+                                Create profiles for kids
+                            </Typography>
+                        }
+                        subheader={
+                            <Typography variant="h5" sx={{marginTop: 2}}>
+                                Send children on adventures with their favourite characters in a space made just for them—free with your membership.
+                            </Typography>
+                        }
+                    />
+                </Card>
+            </Grid>
+        </Grid>
     );
 }
 
