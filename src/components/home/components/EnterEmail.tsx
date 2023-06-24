611b80ef8d3ebb516b47319fc5b39af342d4e1b3
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-25 00:15:51 +0530

    Modify height to minHeight.

--- src/components/home/components/EnterEmail.tsx
+++ src/components/home/components/EnterEmail.tsx
@@ -5,7 +5,7 @@ function EnterEmail(): JSX.Element {
     return (
         <>
             <img src="src/assets/EnterEmail.jpg" alt="Enter email background image." style={{ position: 'absolute', height: '100%', width: '100%', opacity: '0.6', filter: 'blur(1px)'}} />
-            <Card sx={{position: 'relative', height: '100vh', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: 'transparent', color: '#fff'}}>
+            <Card sx={{position: 'relative', minHeight: '100vh', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: 'transparent', color: '#fff'}}>
                 <CardHeader 
                     title={
                         <Typography variant="h3" sx={{fontWeight: 'bolder', marginTop: 10}}>
