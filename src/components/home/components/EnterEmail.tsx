2899a489d00ed558031ea817afee51a2d464aae2
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-24 14:28:54 +0530

    Move email box to a separate component.

--- src/components/home/components/EnterEmail.tsx
+++ src/components/home/components/EnterEmail.tsx
@@ -1,6 +1,7 @@
 import { ChevronRight } from "@mui/icons-material";
 import { Button, Card, CardContent, CardHeader, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import EmailBox from "./EmailBox";
 
 function EnterEmail(): JSX.Element {
     return (
@@ -19,34 +20,7 @@ function EnterEmail(): JSX.Element {
                     </Typography>
                 } />
                 <CardContent>
-                    <Typography variant="h6">
-                        Ready to watch? Enter your email to create or restart your membership.
-                    </Typography>
-                    <div style={{marginTop: 30}}>
-                        <TextField 
-                            label='Email address' 
-                            InputLabelProps={{
-                                style: { color: '#aaa' }
-                            }}
-                            sx={{background: 'rgba(0, 0, 0, 0.7)', border: '1px solid #777', borderRadius: 2, width: '400px'}} />
-                        <Link to='/signup'>
-                            <Button 
-                                variant="contained" 
-                                sx={{
-                                    backgroundColor: '#e50914', 
-                                    ":hover": {backgroundColor: '#bf0811'},
-                                    height: 55,
-                                    width: 180,
-                                    marginLeft: 1,
-                                    fontWeight: 'bold',
-                                    fontSize: 'large',
-                                    textTransform: 'none'
-                                }}
-                            >
-                                Get Started<ChevronRight />
-                            </Button>
-                        </Link>
-                    </div>
+                    <EmailBox />
                 </CardContent>
             </Card>
         </>
