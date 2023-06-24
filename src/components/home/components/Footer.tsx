f666711d9eeb52d843d98b5cf39622178c8c44fb
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-25 00:32:33 +0530

    Add language select menu.

--- src/components/home/components/Footer.tsx
+++ src/components/home/components/Footer.tsx
@@ -1,8 +1,9 @@
 import { Grid, Typography } from "@mui/material";
+import LanguageOption from "./LanguageOption";
 
 function Footer(): JSX.Element {
     return (
-        <Grid container sx={{color: '#fff', background: '#000', minHeight: '100vh'}}>
+        <Grid container sx={{marginTop: 1, color: '#fff', background: '#000', minHeight: '100vh'}}>
             <Grid item xs={12}>
                 <Typography>
                     Questions? Call 000-800-919-1694
@@ -18,7 +19,7 @@ function Footer(): JSX.Element {
 
             </Grid>
             <Grid xs={12}>
-                
+                <LanguageOption />
             </Grid>
             <Grid xs={12}>
                 <Typography>Netflix India</Typography>
