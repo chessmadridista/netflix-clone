14b80262be605e11fdfbd7a1863314937a73aeab
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-25 12:43:09 +0530

    Add navigation to home page on logo click.

--- src/components/login/components/NetflixLogo.tsx
+++ src/components/login/components/NetflixLogo.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 function NetflixLogo(): JSX.Element {
     return (
-        <img
-            src="src/assets/netflix logo.png" alt="Netflix logo" width={170}
-            style={{position: 'absolute', top: '3vh', left: '3vw', zIndex: 1}}
-        />
+        <Link to='/'>
+            <img
+                src="src/assets/netflix logo.png" alt="Netflix logo" width={170}
+                style={{position: 'absolute', top: '3vh', left: '3vw', zIndex: 1}}
+            />    
+        </Link>
+
     );
 }
 
