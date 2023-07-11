215efcd5ea6a8bb81970a4eaf55c74601d486f3e
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-07-11 09:42:16 +0530

    Show border on cards when it is hovered on.

--- src/components/who's watching/components/User.tsx
+++ src/components/who's watching/components/User.tsx
@@ -3,11 +3,12 @@ import UserProps from "../interfaces/UserProps";
 
 function User({id, firstName, imageBackgroundSrc}: UserProps): JSX.Element {
     return (
-        <Card key={id} sx={{background: '#232323'}}>
+        <Card key={id} sx={{background: '#232323',}}>
             <CardMedia
             component="img"
-            height="150px"
+            height="170px"
             image={imageBackgroundSrc}
+            sx={{":hover": {border: '1px solid #fff',}}}
             />
             <CardHeader sx={{color: '#fff'}} 
                 title={
